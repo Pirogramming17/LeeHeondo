2d57d245304ba1a159ec7f5a882a011d009ced6f
Rundo <rundo999@gmail.com>
2022-07-08 21:47:19 +0900

    1번-5단계

--- python_problem/python_problem_2.py
+++ python_problem/python_problem_2.py
@@ -1,4 +1,5 @@
 num = 0
+
 ip = int(input("부를 숫자의 개수를 입력하세요(1,2,3만 입력가능):"))
 
 while True:
@@ -15,8 +16,44 @@ if ip == 1:
     num +=1
     print('playerA : ',num)
 elif ip == 2:
-    num +=2
+    num +=1
+    print('playerA : ',num)
+    num +=1
     print('playerA : ',num)
 else:
-    num +=3
+    num +=1
     print('playerA : ',num)
+    num +=1
+    print('playerA : ',num)
+    num +=1
+    print('playerA : ',num)
+
+ip = int(input("부를 숫자의 개수를 입력하세요(1,2,3만 입력가능):"))
+
+while True:
+    if type(ip) != int:
+        print("정수를 입력하세요")
+
+    elif ip < 1 or ip > 3:
+        print("1,2,3만 입력가능")
+    
+    else:
+        break
+
+if ip == 1:
+    num +=1
+    print('playerA : ',num)
+elif ip == 2:
+    num +=1
+    print('playerA : ',num)
+    num +=1
+    print('playerA : ',num)
+else:
+    num +=1
+    print('playerA : ',num)
+    num +=1
+    print('playerA : ',num)
+    num +=1
+    print('playerA : ',num)
+
+
