f9a6bb8f30bfaab85e10a4c0fdc19d168e325a22
Rundo <rundo999@gmail.com>
2022-07-08 22:55:37 +0900

    1번-8단계

--- python_problem/python_problem_2.py
+++ python_problem/python_problem_2.py
@@ -1,32 +1,39 @@
-num = 0
-player = 'A'
+def brGame():
+    num = 0
+    player = 'A'
 
-def round():
-    while True:
-        try:
-            ip = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력가능):"))
-            if ip < 1 or ip > 3:
-                print("1,2,3만 입력가능")
-            else:
-                return ip
-        except ValueError:
-            print("정수를 입력하세요")
-def record(num):
-    for i in range(round()):
-        num += 1
-        print(f'player{player} : {num}')
-    return num
+    def round(num, player):
+        while True:
+            try:
+                ip = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력가능):"))
+                if ip < 1 or ip > 3:
+                    print("1,2,3만 입력가능")
+                else:
+                    break
+            except ValueError:
+                print("정수를 입력하세요")
+                
+        for i in range(ip):
+            num += 1
+            print(f'player{player} : {num}')
+        return num
+        
+    while num < 31:
+        num = round(num, player)
+        if player == 'A':
+            player = 'B'
+        else:
+            player = 'A'
 
-while num < 31:
-    num = record(num)
     if player == 'A':
         player = 'B'
     else:
-        player = 'A'
+        player = 'A'       
 
-if player == 'A':
-    player = 'B'
-else:
-    player = 'A'       
-print(f'player{player} win!')
+    print(f'player{player} win!')
+    
+
+
+brGame()
+
 
