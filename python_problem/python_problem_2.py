716b50036094bf6ac32ea19ac0e5418b32fcf2d8
Rundo <rundo999@gmail.com>
2022-07-08 22:48:00 +0900

    1번-7단계

--- python_problem/python_problem_2.py
+++ python_problem/python_problem_2.py
@@ -2,17 +2,15 @@ num = 0
 player = 'A'
 
 def round():
-    ip = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력가능):"))
-    try:
-        if ip < 1 or ip > 3:
-            print("1,2,3만 입력가능")
-            round()
-        else:
-            return ip
-    except ValueError:
-        print("정수를 입력하세요")
-        round()
-
+    while True:
+        try:
+            ip = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력가능):"))
+            if ip < 1 or ip > 3:
+                print("1,2,3만 입력가능")
+            else:
+                return ip
+        except ValueError:
+            print("정수를 입력하세요")
 def record(num):
     for i in range(round()):
         num += 1
@@ -25,4 +23,10 @@ while num < 31:
         player = 'B'
     else:
         player = 'A'
+
+if player == 'A':
+    player = 'B'
+else:
+    player = 'A'       
+print(f'player{player} win!')
 
