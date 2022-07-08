33492d77206851293e901f402ec7cc49073662c8
Rundo <rundo999@gmail.com>
2022-07-08 21:38:53 +0900

    1번-3단계

--- python_problem/python_problem_2.py
+++ python_problem/python_problem_2.py
@@ -1,2 +1,12 @@
 num = 0
 ip = int(input("부를 숫자의 개수를 입력하세요(1,2,3만 입력가능):"))
+
+while True:
+    if type(ip) != int:
+        print("정수를 입력하세요")
+
+    elif ip < 1 or ip > 3:
+        print("1,2,3만 입력가능")
+    
+    else:
+        break
