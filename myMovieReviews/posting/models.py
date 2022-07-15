b94ea9e21671b782e29d3a99a9b183f13d3b8b83
Rundo <rundo999@gmail.com>
2022-07-16 02:20:15 +0900

    movie최종

--- myMovieReviews/posting/models.py
+++ myMovieReviews/posting/models.py
@@ -6,20 +6,33 @@ from django.utils import timezone
 class Post(models.Model):
     title = models.CharField(max_length= 100, verbose_name="제목")
     open_date = models.CharField(max_length= 50, verbose_name="개봉연도")
+    
     GENRE_CHOICES = [
         ('ACTION', 'Action'),
         ('COMEDY', 'Comedy'),
         ('THRILLER', 'Thriller'),
         ('ROMANTIC', 'Romantic'),
         ('SF', 'SF'),
+        ('DRAMA', 'Drama'),
+        ('ANIMATION', 'Animation'),
+        ('ADVENTURE', 'Adventure'),
+        ('FANTASY', 'Fantasy'),
+        ('HORROR', 'Horror'),
+        ('MYSTERY', 'Mystery'),
+        ('DOCUMENTARY', 'Documentary'),
         ('CHOICE', '선택'),
+
     ]
     genre = models.CharField(
-        max_length= 10, choices = GENRE_CHOICES,
-        default = 'CHOICE',
+        max_length= 20, verbose_name="장르",choices = GENRE_CHOICES,
+        default = 'CHOICE'
     )
-    star = models.CharField(max_length= 50, verbose_name="별점")
-    running_time = models.CharField(max_length= 50, verbose_name="러닝타임")
+
+    star = models.FloatField(max_length= 50, verbose_name="별점")
+    running_time = models.IntegerField(verbose_name="러닝타임")
+    running_hour = 0
+    running_min = 0
+
     review = models.TextField(verbose_name="리뷰")
     director = models.CharField(max_length= 100, verbose_name="감독")
     actor = models.CharField(max_length= 100, verbose_name="배우")
--- myMovieReviews/posting/views.py
+++ myMovieReviews/posting/views.py
@@ -6,13 +6,17 @@ from .forms import PostForm
 
 # 게시일 기준으로 정렬
 #request: 사용자가 요청하는 모든 정보
-# {'posts': posts} -> 템플릿을 사용하기 위한 매개변수? 'posts'라는 이름으로 쿼리셋 posts를 전달
+# {'post': post} -> 템플릿을 사용하기 위한 매개변수? 'post'라는 이름으로 쿼리셋 post를 전달
 
 def post_list(request):
-    return render(request, 'posting/post_list.html', {})
-
-def post_list(request):
-    posts = Post.objects.filter(created_at__lte=timezone.now()).order_by('created_at') # posts : 쿼리셋의 이름
+    sort = request.GET.get('sort','None')
+    if sort == 'title':
+        posts = Post.objects.all().order_by('title')
+    elif sort ==  'star':
+        posts = Post.objects.all().order_by('-star')
+    else:
+        posts = Post.objects.all().order_by('-running_time')
+    # posts = Post.objects.filter(created_at__lte=timezone.now()).order_by('created_at') # posts : 쿼리셋의 이름
     return render(request, 'posting/post_list.html', {'posts': posts})
 
 def post_detail(request, pk):
@@ -24,7 +28,6 @@ def post_new(request):
         form = PostForm(request.POST)
         if form.is_valid():
             post = form.save(commit=False)
-            # post.author = request.user
             post.created_at = timezone.now()
             post.save()
             return redirect('/')
@@ -38,7 +41,6 @@ def post_edit(request, pk):
         form = PostForm(request.POST, instance=post) #모델 인스턴스로 넘겨주기
         if form.is_valid():
             post = form.save(commit=False)
-            # post.author = request.user
             post.updated_at= timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
@@ -50,3 +52,4 @@ def post_delete(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.delete()
     return redirect('/')
+    
